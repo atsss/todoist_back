ce052a15da21f841e3da9786da11d26b50fb3784
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-03-30 18:13:42 +0900

    set up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def add(params)
+  record = @seed_klass.create!(params)
+
+  yield(record) if block_given?
+end
+
+def set(klass)
+  @seed_klass = klass
+
+  yield
+end
+
+set User do
+  (1..3).each do |i|
+    id = format('%02d', i)
+    add name: "user#{id}", email: "user#{id}@example.com", password: 'passpass', password_confirmation: 'passpass'
+  end
+end
+
+set Task do
+  (1..3).each do |i|
+    id = format('%02d', i)
+    add user: User.first, name: "task#{id}", due_date: Time.zone.now + i.hours
+  end
+end
