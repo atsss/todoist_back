859e0fbf6ada731f624a0b88f2bcb8030f58e135
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-03-30 17:18:01 +0900

    create user and task models

---
+++ app/models/task.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)        not null
+#  name       :string(255)
+#  due_date   :date
+#  done_at    :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tasks_on_user_id  (user_id)
+#
+
+class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :name, :due_date, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+end
