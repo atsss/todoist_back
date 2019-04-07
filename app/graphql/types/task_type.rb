3302171a3fab377fefeaac90022f86a66cb98a89
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-04-07 19:36:47 +0900

    add created_at field to task type

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -4,6 +4,7 @@ module Types
     field :name, String, null: false
     field :due_date, GraphQL::Types::ISO8601DateTime, null: false
     field :done_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :user, UserType, null: false
   end
 end
