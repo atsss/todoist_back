ec5b9d34518e0d9a65c294ebe5e5a312f1ef09bc
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-04-11 08:04:23 +0900

    create update_task mutation

---
+++ app/graphql/mutations/update_task.rb
@@ -0,0 +1,16 @@
+module Mutations
+  class UpdateTask < BaseMutation
+    argument :id, ID, required: true
+    argument :name, String, required: false
+    argument :due_date, GraphQL::Types::ISO8601DateTime, required: false
+
+    type Types::TaskType
+
+    def resolve(id: nil, name: nil, due_date: nil)
+      task = Task.find(id)
+      task.update!({ name: name, due_date: due_date }.compact)
+
+      task
+    end
+  end
+end
