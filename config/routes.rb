1c268147559019b004b6ee44d27addff16eebc68
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-16 11:47:20 +0900

    set up routing for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
-  post "/graphql", to: "graphql#execute"
 
-  # GraphQL で認証するのでコメントアウト
-  # mount_devise_token_auth_for 'Account', at: 'auth'
+  mount_devise_token_auth_for 'Account', at: 'auth'
+
+  post "/graphql", to: "graphql#execute"
 end
