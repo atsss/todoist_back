7e7e71ac02ce459f70ce1262c213fdf7dad6df43
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-17 12:17:27 +0900

    get only tasks of current user

--- app/graphql/resolvers/tasks.rb
+++ app/graphql/resolvers/tasks.rb
@@ -3,7 +3,10 @@ module Resolvers
     type [Types::Objects::TaskType], null: false
 
     def resolve
-      ::Task.not_done.includes(:user)
+      ::Task
+        .where(user: context[:current_user])
+        .not_done
+        .includes(:user)
     end
   end
 end
--- spec/graphql/resolvers/tasks_spec.rb
+++ spec/graphql/resolvers/tasks_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Resolvers::Tasks, type: :graphql do
-  let!(:tasks) { create_list(:task, 3) }
-  let(:result) { perform }
+  let(:user) { create(:user) }
+  let(:another_user) { create(:user) }
+  let!(:tasks) { create_list(:task, 3, user: user) }
+  let(:result) { perform(context: { current_user: user }) }
 
-  it do
+  it 'get tasks of current user' do
+    create_list(:task, 2, user: another_user)
+
+    expect(result.count).to eq 3
+  end
+
+  it 'get tasks witch is not done' do
     tasks.first.done!
+
     expect(result).to eq tasks.last(2)
   end
 end
