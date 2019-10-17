1688e3c4ac82f485cbffc1807c0a68f918404399
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-17 12:15:08 +0900

    fix mutation_type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,6 @@ module Types
     field :create_task, mutation: Mutations::CreateTask
     field :create_user, mutation: Mutations::CreateUser
     field :done_task, mutation: Mutations::DoneTask
-    field :signin_user, mutation: Mutations::SignInUser
     field :update_task, mutation: Mutations::UpdateTask
   end
 end
