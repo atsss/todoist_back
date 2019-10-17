6c3831d9661f4bcd1b5b43dd3a4fecc1b981aeab
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-17 11:49:34 +0900

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,22 @@ end
 set User do
   (1..3).each do |i|
     id = format('%02d', i)
-    add name: "user#{id}", email: "user#{id}@example.com", password: 'passpass', password_confirmation: 'passpass'
+    add name: "user#{id}"
+  end
+end
+
+set Account do
+  (1..3).each do |i|
+    id = format('%02d', i)
+    add user_id: i, email: "test#{id}@example.com", password: 'passpass'
   end
 end
 
 set Task do
-  (1..3).each do |i|
+  (1..9).each do |i|
     id = format('%02d', i)
-    add user: User.first, name: "task#{id}", due_date: Time.zone.now + i.hours
+    user = i.even? ? User.first : User.second
+
+    add user: user, name: "task#{id}", due_date: Time.zone.now + i.hours
   end
 end
