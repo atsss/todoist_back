5bde855ea63f5d00dca30ab66336b3589ef7d6e0
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-10-19 20:49:23 +0900

    fix graphql_helper

--- spec/support/graphql_helper.rb
+++ spec/support/graphql_helper.rb
@@ -1,6 +1,4 @@
 def perform(klass: described_class, args: {}, context: {})
-  context = { session: {} }.merge(context)
-
   return klass.new(object: nil, context: context).resolve(args) if args.present?
 
   klass.new(object: nil, context: context).resolve
