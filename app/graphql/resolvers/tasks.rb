514966afb58342af59850b36b61141ebbc4b6247
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-11-01 12:14:35 +0900

    fix tasks resolver to get correct schedule

--- app/graphql/resolvers/tasks.rb
+++ app/graphql/resolvers/tasks.rb
@@ -8,7 +8,7 @@ module Resolvers
         .for_today
         .not_done
         .order_by_time
-        .includes(:user, :schedule, :schedules, :tags)
+        .includes(:user, :schedules, :tags) # FIXME: schedule のメソッド分 N+1 が発生する
     end
   end
 end
