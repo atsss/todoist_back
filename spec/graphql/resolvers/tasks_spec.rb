7236f9614f24f856f6c1c2748da10c2c5f48a075
atsushi0623itoh <atsushi0623itoh@gmail.com>
2019-11-11 21:42:00 +0900

    fix tasks resolver spec

--- spec/graphql/resolvers/tasks_spec.rb
+++ spec/graphql/resolvers/tasks_spec.rb
@@ -3,18 +3,28 @@ require 'rails_helper'
 RSpec.describe Resolvers::Tasks, type: :graphql do
   let(:user) { create(:user) }
   let(:another_user) { create(:user) }
-  let!(:tasks) { create_list(:task, 3, user: user) }
+  let!(:tasks) { create_list(:task, 2, :with_schedule, user: user) }
   let(:result) { perform(context: { current_user: user }) }
 
   it 'get tasks of current user' do
-    create_list(:task, 2, user: another_user)
+    create(:task, :with_schedule, user: another_user)
 
-    expect(result.count).to eq 3
+    expect(result).to eq tasks
   end
 
   it 'get tasks witch is not done' do
-    tasks.first.done!
+    tasks.first.done!(score: 8)
 
-    expect(result).to eq tasks.last(2)
+    expect(result).to eq [tasks.last]
+  end
+
+  context 'on 11th Nov 2019' do
+    before { travel_to('2019-11-11') }
+
+    it 'get tasks witch is for today' do
+      tasks.first.schedule.update!(kind: :sunday)
+
+      expect(result).to eq [tasks.last]
+    end
   end
 end
